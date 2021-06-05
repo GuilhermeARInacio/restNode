9eea7910528d71755fc20dc3de4b2fa9e9cd27d2
Guilherme Alves <guilhermeari@outlook.com>
2021-06-05 15:45:40 -0300

    teste

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -26,7 +26,7 @@ module.exports = app => {
     app.delete('/atendimentos/:id', (req, res) => {
         const id = parseInt(req.params.id)
 
-        Atendimento.deleta(id, res)
+        Atendimento.deleta(id, res) 
     })
 }
 
